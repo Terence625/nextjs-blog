51e59f3285f272dc792a9e43167b29a51e32c4a3
Terence625 <terencetianterence@gmail.com>
2022-03-07 21:53:52 +0800

    [Terence] convert to typescript and add new favicon.ico

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import React from "react";
 
 const name = "Terence Tian";
 export const siteTitle = "Terence's Blog";
 
-export default function Layout({ children, home }) {
+export default function Layout({ children, home }: { children: React.ReactNode; home?: boolean }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
         <meta name="description" content="Learn how to build a personal website using Next.js" />
         <meta
           property="og:image"
